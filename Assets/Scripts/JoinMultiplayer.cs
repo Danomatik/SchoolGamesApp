2b3326903af9dc2a5872cb6885d2cc3d48c988e0
Mom0bondi <129185182+Mom0bondi@users.noreply.github.com>
2025-10-22 00:40:14 +0200

    menu + physical dice

---
+++ Assets/Scripts/JoinMultiplayer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JoinMultiplayer : MonoBehaviour
+{
+    public void JoinMultiplayerMode()
+	{
+		SceneManager.LoadScene("MainScene");
+	}
+}
