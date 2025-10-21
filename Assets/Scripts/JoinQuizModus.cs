a3cca863873db5792110cba7a5aa4d7db8984d0f
chidi210621 <chi210621@spengergasse.at>
2025-10-21 20:23:49 +0200

    added JoinQuiz Script to quiz button

---
+++ Assets/Scripts/JoinQuizModus.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class JoinQuizModus : MonoBehaviour
+{
+    public void JoinQuizMode()
+    {
+        SceneManager.LoadScene("Quiz");
+    }
+}
