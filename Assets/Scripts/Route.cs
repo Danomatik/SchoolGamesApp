9dc4f39464cbf35695b08415ef80bc835b23195d
Mom0bondi <129185182+Mom0bondi@users.noreply.github.com>
2025-10-24 18:40:08 +0200

    Fixed bugs, resized field, new player model

--- Assets/Scripts/Route.cs
+++ Assets/Scripts/Route.cs
@@ -6,25 +6,4 @@ public class Route : MonoBehaviour
 {
     Transform[] childObjects;
     public List<Transform> childNodeList = new List<Transform>();
-
-    void Start()
-    {
-        FillNodes();
-    }
-
-    void FillNodes()
-    {
-        childObjects = GetComponentsInChildren<Transform>();
-        childNodeList.Clear();
-
-        foreach (Transform child in childObjects)
-        {
-            if (child != this.transform)
-            {
-                childNodeList.Add(child);
-            }
-        }
-
-       
-    }
 }
